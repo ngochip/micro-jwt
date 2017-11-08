b1b37ad8249cb00d9ec1246a0f09fe854d336631
Đỗ Ngọc <ngochip136@gmail.com>
2017-11-09 02:27:38 +0700

    dev.. add method verify, get data, get header

--- Jwt.php
+++ Jwt.php
@@ -6,6 +6,8 @@ use Yii;
 use yii\base\Component;
 use yii\base\InvalidParamException;
 use sizeg\jwt\Jwt as SizeGJWT;
+use Lcobucci\JWT\Signer\Keychain;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
 
 /**
  * JSON Web Token implementation, based on this library:
@@ -16,10 +18,84 @@ use sizeg\jwt\Jwt as SizeGJWT;
  */
 class Jwt extends Component
 {
+	public $privateKey;
+	public $publicKey;
+	public $passparse;
 	public $ttl;
 	public $ttl_refresh;
+	private $token;
+	private $signer;
+	private $keychain;
+	private $sizeGJWT;
 
-	public function getToken($info){
-		echo "xxxx";
+	public function __construct(){
+		$this->signer = new Sha256();
+    	$this->keychain = new Keychain();
+    	$this->sizeGJWT = new SizeGJWT();
 	}
+
+	/*
+	* tạo mới token
+	*/
+	public function getToken($info, $uid = null){
+		$this->token = $this->_getToken($info, $uid);
+		return (string) $this->token;
+	}
+
+	/*
+	*
+	*/
+	public function setToken($token){
+		$this->token = $this->sizeGJWT->getParser()->parse((string) $token);
+	}
+	/*
+	*
+	* verify lại token
+	*/
+	public function verify(){
+		$verify = $this->token->verify($this->signer, $this->keychain->getPublicKey('file://'.$this->publicKey));
+		return $verify;
+	}
+
+	/*
+	*
+	*/
+
+	public function getInfo($claimName = 'info'){
+		$this->token->getClaims();
+		return $this->token->getClaim($claimName);
+	}
+
+	/*
+	*
+	*/
+
+	public function getHeader($headerName){
+		$this->token->getHeaders();
+		return $this->token->getHeader($headerName);
+	}
+
+	/*
+	* private function
+	*/
+	private function _getToken($info, $uid = null){
+    	$builder = $this->sizeGJWT->getBuilder()->setIssuer(Yii::$app->homeUrl)
+        // ->setAudience('http://example.org')
+        ->setIssuedAt(time())
+        ->setNotBefore(time())
+        ->setExpiration(time() + $this->ttl)
+        ->set('info', $info);
+        if($uid == null){
+        	$builder->setId('uid', $uid);
+        }
+        $builder->sign(
+        	$this->signer, 
+        	$this->keychain->getPrivateKey('file://'.$this->privateKey, $this->passparse)
+       	);
+
+       	$token = $builder->getToken();
+
+       	return $token;
+	}
+
 }
