d08f4350a5accd328e64726aa62842cdadb04b62
Do Duy Ngoc <do.duy.ngoc@mqsolutions.vn>
2017-11-09 14:24:30 +0700

    add redis

--- Jwt.php
+++ Jwt.php
@@ -5,6 +5,7 @@ namespace ngochip\jwt;
 use Yii;
 use yii\base\Component;
 use yii\base\InvalidParamException;
+use yii\redis\Connection as Redis;
 use sizeg\jwt\Jwt as SizeGJWT;
 use Lcobucci\JWT\Signer\Keychain;
 use Lcobucci\JWT\Signer\Rsa\Sha256;
@@ -23,21 +24,30 @@ class Jwt extends Component
 	public $passparse;
 	public $ttl;
 	public $ttl_refresh;
+	public $redis_config;
+
 	private $token;
 	private $signer;
 	private $keychain;
 	private $sizeGJWT;
 
+	CONST PRE_BLACKLIST_KEY = "auth:blackList:";
+
 	public function __construct(){
 		$this->signer = new Sha256();
     	$this->keychain = new Keychain();
     	$this->sizeGJWT = new SizeGJWT();
+    	$this->_initRedis();
 	}
 
 	/*
 	* tạo mới token
 	*/
-	public function getToken($info, $uid = null){
+	public function getToken($info = null, $uid = null){
+		if($info == null){
+			return (string) $this->token;
+		}
+
 		$this->token = $this->_getToken($info, $uid);
 		return (string) $this->token;
 	}
@@ -58,17 +68,30 @@ class Jwt extends Component
 	}
 
 	/*
-	*
+	* tạo token mới khi hết hạn
 	*/
+	public function refreshToken(){
+		$expire 		= $this->getInfo("exp");
+		$iat 			= $this->getInfo('iat'); //time that the token was issue
+		$ttl 			= $this->redis_config['ttl'];
+		$ttlRefresh 	= $this->redis_config['ttl'];
+		$remain 		= time() - $expire;
+		$remainRefreshTime = time() - ($iat + $ttlRefresh);
+		$info 			= $this->getInfo();
 
+		if($remainRefreshTime <= 0){
+			return false; //hết hạn refresh token
+		}
+		if($remain > 0){
+			$this->_addToBlackList();
+		}
+		return $this->getToken($info);
+	}
+	//get claim from token
 	public function getInfo($claimName = 'info'){
 		$this->token->getClaims();
 		return $this->token->getClaim($claimName);
 	}
-
-	/*
-	*
-	*/
 
 	public function getHeader($headerName){
 		$this->token->getHeaders();
@@ -89,13 +112,38 @@ class Jwt extends Component
         	$builder->setId('uid', $uid);
         }
         $builder->sign(
-        	$this->signer, 
+        	$this->signer,
         	$this->keychain->getPrivateKey('file://'.$this->privateKey, $this->passparse)
        	);
-
        	$token = $builder->getToken();
-
        	return $token;
 	}
 
+	private function _initRedis(){
+		$this->Redis = new Redis();
+		$config = $this->redis_config;
+		$this->Redis->hostname = isset($config['host']) ? $config['host'] : "localhost";
+		$this->Redis->port = isset($config['port']) ? $config['port'] : 6379;
+		$this->Redis->password = isset($config['password']) ? $config['password'] : null;
+		$this->Redis->database = isset($config['database']) ? $config['database'] : 0;
+	}
+
+	private function _getRedisKeyName($key){
+		return self::PRE_BLACKLIST_KEY.md5($key);
+	}
+	private function _addToBlackList(){
+		$expire = $this->getInfo("exp");
+		$tokenString = $this->getToken();
+		$keyName = $this->_getRedisKeyName($tokenString);
+		$remain = time() - $expire;
+
+		return $this->Redis->executeCommand("SET", [$keyName, '1','NX','EX',$remain]);
+	}
+	private function _inBlackList(){
+		$tokenString = $this->getToken();
+		$keyName = $this->_getRedisKeyName($tokenString);
+		return $this->Redis->executeCommand("EXISTS", [$keyName]);
+	}
+
 }
+
